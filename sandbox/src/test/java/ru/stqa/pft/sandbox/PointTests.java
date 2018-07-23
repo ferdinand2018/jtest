6797b378dc350fa7a31b39514fcda8ff0597ba0d
Максим <conowalow.maksim@yandex.ru>
2018-07-23 03:56:33 +0300

    Исправлен тестовый класс, проверяющий, что расстояне между точками вычисляется правильно

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -7,10 +7,8 @@ public class PointTests {
   @Test
   public void testDistance(){
     /*1я проверка*/
-    Point p = new Point(12, 2, 2, 54);
-    Assert.assertEquals(p.distance(), 52.952809179494906);
-    /*2я проверка (с отрицательным числом)*/
-    Point p1 = new Point(-12, 22, 21, 154);
-    Assert.assertEquals(p1.distance(), 136.0624856453828);
+    Point p1 = new Point(12, 2);
+    Point p2 = new Point(2, 54);
+    Assert.assertEquals(p1.distance(p2), 52.952809179494906);
   }
 }
